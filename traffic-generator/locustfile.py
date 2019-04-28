6f047d2676032e6fabd56036ff78b1221f247c8c
ishrivatsa <ushrivatsa@gmail.com>
2019-04-27 23:05:51 -0700

    Update locust file

--- traffic-generator/locustfile.py
+++ traffic-generator/locustfile.py
@@ -1,4 +1,4 @@
-from locust import HttpLocust, TaskSet, task, TaskSequence, seq_task
+from locust import HttpLocust, TaskSet, task, TaskSequence, seq_task, Locust
 import random
 
 users = ["eric", "phoebe", "dwight", "han"] ##User 'rob' does not exist. This to simulate a failure with incorrect user auth
@@ -8,12 +8,20 @@ products = []
 userid = ""
 
 
+def my_task(l):
+    print("hello")
+
+def new_task(l):
+    print("new hello")
+
 class GuestUserBrowsing(TaskSequence):
 
     def on_start(self):
         self.getProducts()
 
-    def getProducts():
+    @task
+    def getProducts(self):
+        print("Guest User - Get Products")
         self.client.get("/products")
 
 class AuthUserBrowsing(TaskSequence):
@@ -24,40 +32,41 @@ class AuthUserBrowsing(TaskSequence):
     #def on_stop(self):
      #   self.logout()
     
-    @seq_task(1)
+   # @seq_task(1)
     @task(1)
     def login(self):
         user = random.choice(users)
         body = self.client.post("/login/", json={"username": user, "password":"vmware1!"}).json()
         userid = body["token"]
 
-    @seq_task(2)
+    #@seq_task(2)
     @task(1)
     def getProducts(self):
         self.client.get("/products").json()["data"]
 
-    @seq_task(3)
+    #@seq_task(3)
     @task(2)
     def getProduct(self):
+        print("Auth User - Getting products")
         products = self.listCatalogItems()
         id = random.choice(products)
         product = self.client.get("/products/"+ id).json()
         products.clear()
 
     
-    @seq_task(4)
-    @task(4)
-    def addToCart(self):
-        self.listCatalogItems()
-        productid = random.choice(products)
-        cart = self.client.post("/cart/item/add/" + userid, json={
-                  "name": "test_" + productid,
-                  "price": "100",
-                  "shortDescription": "Test add to cart",
-                  "quantity": random.randint(1,4),
-                  "itemid": productid
-                })
-        print(cart)
+    # @seq_task(4)
+    # @task(4)
+    # def addToCart(self):
+    #     self.listCatalogItems()
+    #     productid = random.choice(products)
+    #     cart = self.client.post("/cart/item/add/" + userid, json={
+    #               "name": "test_" + productid,
+    #               "price": "100",
+    #               "shortDescription": "Test add to cart",
+    #               "quantity": random.randint(1,4),
+    #               "itemid": productid
+    #             })
+    #     print(cart)
 
     def listCatalogItems(self):
         items = self.client.get("/products").json()["data"]
@@ -69,11 +78,13 @@ class AuthUserBrowsing(TaskSequence):
     def index(self):
         self.client.get("/")
 
+
 class UserBehavior(TaskSet):
     tasks = {AuthUserBrowsing:1, GuestUserBrowsing:2}
-
+    #tasks = {my_task:1, new_task:2}
 
 class WebSiteUser(HttpLocust):
+
     task_set = UserBehavior
     min_wait = 2000
     max_wait = 10000
